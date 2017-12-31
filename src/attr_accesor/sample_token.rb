b4a16353916cfd6978a16f77fb024f26cb089667
ItinoseSan <jkekun@gmail.com>
2017-12-31 13:58:47 +0900

    fix typo "Sever-> Server"

--- src/attr_accesor/sample_token.rb
+++ src/attr_accesor/sample_token.rb
@@ -1,11 +1,11 @@
-class LocalSever
+class LocalServer
   attr_accessor :port
   def initialize(port)
     @port = port
   end
 end
 
-localhost = LocalSever.new(3000)
+localhost = LocalServer.new(3000)
 
 p localhost.port
 
